15fa136c522745fe1d50757ef7ca7155d20254be
Vulena <vcrystals18@gmail.com>
2020-07-15 13:38:48 -0700

    Creation of Xcode project

---
+++ GSO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GSO
+//
+//  Created by Apple on 7/15/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
